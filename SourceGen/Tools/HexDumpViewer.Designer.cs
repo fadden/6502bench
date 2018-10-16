25b7faf43b718c9a0097d38ae11bd63d60b98f1c
Andy McFadden <fadden@fadden.com>
2018-10-15 17:24:32 -0700

    Hex dump shows file offset, not address

--- SourceGen/Tools/HexDumpViewer.Designer.cs
+++ SourceGen/Tools/HexDumpViewer.Designer.cs
@@ -68,7 +68,7 @@ namespace SourceGen.Tools {
             // 
             // columnHeader1
             // 
-            this.columnHeader1.Text = "Address  0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F  ASCII";
+            this.columnHeader1.Text = "Offset   0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F  ASCII";
             this.columnHeader1.Width = 455;
             // 
             // charConvComboBox
