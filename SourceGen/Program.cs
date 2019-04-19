ab590c5a2a82ed29d48f21e234db22322de9d135
Andy McFadden <fadden@fadden.com>
2019-04-19 14:54:10 -0700

    Version 1.1.0

--- SourceGen/Program.cs
+++ SourceGen/Program.cs
@@ -21,7 +21,7 @@ namespace SourceGen {
     static class Program {
         // Version number applied to the program as a whole.
         public static readonly CommonUtil.Version ProgramVersion =
-            new CommonUtil.Version(1, 1, 0, CommonUtil.Version.PreRelType.Dev, 3);
+            new CommonUtil.Version(1, 1, 0, CommonUtil.Version.PreRelType.Final, 0);
 
         /// <summary>
         /// The main entry point for the application.
