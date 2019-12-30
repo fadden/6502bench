f157fbbfd307509b9670536d6d3aa0c1b71a545a
Andy McFadden <fadden@fadden.com>
2019-12-30 14:09:18 -0800

    Add regression test for data analysis bug
    The uncategorized data scanner isn't supposed to create strings or
    ".fill" directives that straddle labels, long comments, notes,
    visualizations, or ORG directives.  The test for crossing an ORG
    directive is incomplete, and doesn't correctly handle no-op ORGs
    (where the new address is the same as the old address).
    
    The code generator doesn't output ORGs that are hidden inside other
    things, so we're not generating bad code, but it looks funny on
    screen and may cause problems later on.  The 2004-numeric-types test
    has the basic .align/.fill/.bulk directive tests, and now has an
    extended set of tests for uncategorized data region splitting.

---
+++ SourceGen/SGTestData/2004-numeric-types.cs
@@ -0,0 +1,58 @@
+﻿/*
+ * Copyright 2019 faddenSoft
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+using System;
+using System.Collections.ObjectModel;
+
+using PluginCommon;
+
+namespace RuntimeData.Test2004 {
+    public class VisTest2004 : MarshalByRefObject, IPlugin, IPlugin_Visualizer {
+        // IPlugin
+        public string Identifier {
+            get { return "Test2004 Dummy Visualizer"; }
+        }
+
+        // Visualization identifiers; DO NOT change or projects that use them will break.
+        private const string VIS_DUMMY = "dummy";
+
+        // Visualization descriptors.
+        private VisDescr[] mDescriptors = new VisDescr[] {
+            new VisDescr(VIS_DUMMY, "Dummy", VisDescr.VisType.Bitmap,
+                new VisParamDescr[] { })
+        };
+
+
+        // IPlugin
+        public void Prepare(IApplication appRef, byte[] fileData, AddressTranslate addrTrans) { }
+
+        // IPlugin
+        public void Unprepare() { }
+
+        // IPlugin_Visualizer
+        public VisDescr[] GetVisGenDescrs() {
+            return mDescriptors;
+        }
+
+        // IPlugin_Visualizer
+        public IVisualization2d Generate2d(VisDescr descr,
+                ReadOnlyDictionary<string, object> parms) {
+            VisBitmap8 vb = new VisBitmap8(1, 1);
+            vb.AddColor(0, 0, 0, 0);
+            vb.SetPixelIndex(0, 0, 0);
+            return vb;
+        }
+    }
+}
