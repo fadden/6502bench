c0de0a8844c81a73005ecffefd0a067a0c690403
Andy McFadden <fadden@fadden.com>
2020-03-13 13:58:52 -0700

    Allow custom colors in Notes
    The data structure and project file have always supported arbitrary
    ARGB colors.  Now the editor does as well.

--- SourceGen/WpfGui/EditNote.xaml.cs
+++ SourceGen/WpfGui/EditNote.xaml.cs
@@ -14,14 +14,16 @@
  * limitations under the License.
  */
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
+using System.Globalization;
 using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
+
+using CommonWPF;
 
 namespace SourceGen.WpfGui {
     /// <summary>
@@ -38,12 +40,38 @@ namespace SourceGen.WpfGui {
         /// </summary>
         public string UserInput {
             get { return mUserInput; }
-            set {
-                mUserInput = value;
-                OnPropertyChanged();
-            }
+            set { mUserInput = value; OnPropertyChanged(); }
         }
         private string mUserInput;
+
+        public string CustomColorStr {
+            get { return mCustomColorStr; }
+            set {
+                mCustomColorStr = value;
+                OnPropertyChanged();
+                UpdateCustomColor();
+                if (mIsInitialized) {
+                    Debug.WriteLine("SET CUSTOM");
+                    IsCustomChecked = true;
+                }
+            }
+        }
+        private string mCustomColorStr;
+
+        public bool IsCustomChecked {
+            get { return mIsCustomChecked; }
+            set { mIsCustomChecked = value; OnPropertyChanged(); }
+        }
+        private bool mIsCustomChecked;
+
+        public Brush CustomColorBrush {
+            get { return mCustomColorBrush; }
+            set { mCustomColorBrush = value; OnPropertyChanged(); }
+        }
+        private Brush mCustomColorBrush = Brushes.Transparent;
+
+        // This is static so it carries over between edits.
+        private static Color mCustomColor = Helper.ZeroColor;
 
         // Highlight color palette.  Unless the user has funky theme, the color will be
         // replacing a white background, and will be overlaid with black text, so should
@@ -60,6 +88,8 @@ namespace SourceGen.WpfGui {
             Colors.Orange
         };
         private RadioButton[] mColorButtons;
+
+        private bool mIsInitialized = false;
 
         // INotifyPropertyChanged implementation
         public event PropertyChangedEventHandler PropertyChanged;
@@ -96,16 +126,32 @@ namespace SourceGen.WpfGui {
             // Configure radio buttons.
             colorDefaultRadio.IsChecked = true;
             if (Note != null) {
+                bool found = false;
                 Color curColor = Note.BackgroundColor;
                 for (int i = 0; i < sColors.Length; i++) {
                     if (sColors[i].Equals(curColor)) {
                         mColorButtons[i].IsChecked = true;
+                        found = true;
                         break;
                     }
                 }
+
+                if (!found) {
+                    IsCustomChecked = true;
+                    mCustomColor = curColor;
+                }
+            }
+
+            if (mCustomColor.A == 0xff) {
+                CustomColorStr = string.Format("#{0:X2}{1:X2}{2:X2}",
+                    mCustomColor.R, mCustomColor.G, mCustomColor.B);
+            } else {
+                CustomColorStr = string.Format("#{0:X2}{1:X2}{2:X2}{3:X2}",
+                    mCustomColor.A, mCustomColor.R, mCustomColor.G, mCustomColor.B);
             }
 
             inputTextBox.Focus();
+            mIsInitialized = true;
         }
 
         // Handle Ctrl+Enter as a way to close the dialog, since plain Enter just
@@ -119,22 +165,68 @@ namespace SourceGen.WpfGui {
 
         private void Window_Closing(object sender, CancelEventArgs e) {
             if (DialogResult != true) {
-                Debug.WriteLine("Skip it");
                 return;
             }
 
             if (string.IsNullOrEmpty(UserInput)) {
                 Note = null;
             } else {
-                Color bkgndColor = Colors.Fuchsia;
-                for (int i = 0; i < mColorButtons.Length; i++) {
-                    if (mColorButtons[i].IsChecked == true) {
-                        bkgndColor = sColors[i];
-                        break;
+                Color bkgndColor = Colors.Fuchsia;  // make it obvious if we screw up
+                if (IsCustomChecked) {
+                    if (Validation.GetHasError(customColorBox)) {
+                        bkgndColor = Helper.ZeroColor;
+                    } else {
+                        bkgndColor = mCustomColor;
+                    }
+                } else {
+                    for (int i = 0; i < mColorButtons.Length; i++) {
+                        if (mColorButtons[i].IsChecked == true) {
+                            bkgndColor = sColors[i];
+                            break;
+                        }
                     }
                 }
                 Note = new MultiLineComment(UserInput, bkgndColor);
             }
         }
+
+        private void UpdateCustomColor() {
+            Color cl;
+            try {
+                cl = (Color)ColorConverter.ConvertFromString(CustomColorStr);
+            } catch (FormatException ex) {
+                // no dice
+                Debug.WriteLine("Unable to convert color '" + CustomColorStr + "': " + ex.Message);
+                CustomColorBrush = Brushes.Transparent;
+                mCustomColor = Helper.ZeroColor;
+                return;
+            }
+
+            mCustomColor = cl;
+            CustomColorBrush = new SolidColorBrush(cl);
+        }
     }
+
+    #region Validation rules
+
+    public class ColorRule : ValidationRule {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo) {
+            string strValue = Convert.ToString(value);
+            if (strValue == null) {
+                return new ValidationResult(false, "could not convert to string");
+            }
+            if (string.IsNullOrEmpty(strValue)) {
+                return new ValidationResult(false, "empty color string");
+            }
+
+            try {
+                ColorConverter.ConvertFromString(strValue);
+                return ValidationResult.ValidResult;
+            } catch (Exception) {
+                return new ValidationResult(false, "invalid color value");
+            }
+        }
+    }
+
+    #endregion Validation rules
 }
