65b960d78bf8d78ceb5a610e96454ddd8048a442
Andy McFadden <fadden@fadden.com>
2020-03-24 14:02:30 -0700

    Version 1.6.0-alpha1

--- SourceGen/App.xaml.cs
+++ SourceGen/App.xaml.cs
@@ -25,6 +25,6 @@ namespace SourceGen {
         /// SourceGen version number.
         /// </summary>
         public static readonly CommonUtil.Version ProgramVersion =
-            new CommonUtil.Version(1, 6, 0, CommonUtil.Version.PreRelType.Dev, 1);
+            new CommonUtil.Version(1, 6, 0, CommonUtil.Version.PreRelType.Alpha, 1);
     }
 }
