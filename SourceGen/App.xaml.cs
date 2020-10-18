cac03e6a251e2dd4454c11ce4bd614774cd481b1
Andy McFadden <fadden@fadden.com>
2020-10-18 13:43:44 -0700

    Version 1.7.3-dev2

--- SourceGen/App.xaml.cs
+++ SourceGen/App.xaml.cs
@@ -25,6 +25,6 @@ namespace SourceGen {
         /// SourceGen version number.
         /// </summary>
         public static readonly CommonUtil.Version ProgramVersion =
-            new CommonUtil.Version(1, 7, 3, CommonUtil.Version.PreRelType.Dev, 1);
+            new CommonUtil.Version(1, 7, 3, CommonUtil.Version.PreRelType.Dev, 2);
     }
 }
