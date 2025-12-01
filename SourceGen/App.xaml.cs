03ea2fc2c8bf411b804868b5ffa71010916eb676
Andy McFadden <fadden@fadden.com>
2025-11-30 16:04:04 -0800

    Version 1.10.1-dev4

--- SourceGen/App.xaml.cs
+++ SourceGen/App.xaml.cs
@@ -25,6 +25,6 @@ namespace SourceGen {
         /// SourceGen version number.
         /// </summary>
         public static readonly CommonUtil.Version ProgramVersion =
-            new CommonUtil.Version(1, 10, 1, CommonUtil.Version.PreRelType.Dev, 3);
+            new CommonUtil.Version(1, 10, 1, CommonUtil.Version.PreRelType.Dev, 4);
     }
 }
